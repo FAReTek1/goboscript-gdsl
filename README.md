f17a737f5981a704d3ffc2091103afe4f7d4927d
r <ldashunderscoreo@gmail.com>
2025-02-16 15:13:53 +0000

    chore: version stuffs

---
+++ README.md
@@ -0,0 +1,4 @@
+# goboscript-gdsl
+
+This is a package that deals with the parsing of the gdsl.txt file for goboscript. It is not officially used and is
+simply 'forked' from the original parser.
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='goboscript-gdsl',
+    version='0.0.0',
+    packages=['goboscript_gdsl'],
+    url='https://github.com/faretek1/goboscript-gdsl',
+    license=open("LICENSE").read(),
+    author='faretek1',
+    author_email='',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
+)
